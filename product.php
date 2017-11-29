301667abb08df271be228df3fa619de29733de0a
Tajhna McCourtie <tmc253@hotmail.com>
2017-11-28 22:09:10 -0500

    updated product.php

--- product.php
+++ product.php
@@ -12,17 +12,52 @@
       <!-- this floats to the left with categories -->
       <div class="sidebar">
       this is sidebar
-      <div class="catlist">
-          call for list
-          <div>
+     <div class="catlist">
+         call for the categories
+          
+</div>
       </div> 
 
        <!-- area for items to display to user-->
       <div class="item-container">
-      this is container
-         <div class="items">
-             
-             <div>
+          <!--left side of the container with Image-->
+         <div class="left">
+                 <div class="imgebox">
+                 <img  src= "css/img/google.jpg" alt="Dog">
+                </div>
+             </div>
+                 
+             <!-- Right side of the container with description-->
+             <div class="right"> 
+             <div class="row">
+                 <span>Category</span>
+                 <div id="name">
+                          <h3>Name:</h3> 
+                 </div> 
+            </div> 
+                
+            <div class="row">
+               <div id="descript">
+                      <h3>Description:</h3>
+              </div>
+            </div>
+            
+            <div class="row">
+               <div id="price">
+               <h3>Price:</h3>
+            </div> 
+          </div>
+
+           <div class="row">
+               <div id="quan">
+                  <h3> Quantity:</h3>
+                  <button type="button">Update</button>
+              </div> 
+         </div>
+
+
+    
+          </div>
       </div>
 
 
