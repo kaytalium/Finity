a071e62c8a9d7f9bc6909d1af28d3e8d51266472
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-11-30 10:18:06 -0500

    modify detail page

--- product/details.php
+++ product/details.php
@@ -16,34 +16,39 @@
     <div class="right"> 
         <div class="row topmost">
             <span><?php echo $detailItem->get_category(); ?></span>
-            <div id="name"><i class="fa fa-pencil" aria-hidden="true"></i>
-                <h3><?php echo $detailItem->get_name(); ?></h3>
+            <div id="name">
+                <p>
+                    <?php echo $detailItem->get_name(); ?>
+                </p>
+                <i class="fa fa-pencil" aria-hidden="true"></i>
             </div> 
         </div> 
                         
         <div class="row"> 
-            <div id="descript"><i class="fa fa-pencil" aria-hidden="true"></i>
-                
+            <div id="descript">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
                 <p><?php echo $detailItem->get_description(); ?></p> 
             </div>
         </div>
                     
         <div class="row">
             <div id="price"><i class="fa fa-pencil" aria-hidden="true"></i>
-                <h3><?php echo $detailItem->get_formatted_price(); ?></h3>
+                <p>Price: <?php echo $detailItem->get_formatted_price(); ?></p>
             </div> 
         </div>
 
         <div class="row">
             <div id="quan"><i class="fa fa-pencil" aria-hidden="true"></i>
-                <h3>Quantity: <?php echo $detailItem->get_unit(); ?></h3>
+                <p>Quantity: <?php echo $detailItem->get_unit(); ?></p>
+            </div> 
        </div> 
        <div class="row">
             <div id="type"><i class="fa fa-pencil" aria-hidden ="true"></i> 
-            <h3>Type: <?php echo $detailItem->get_type(); ?></h3>        
-           <div>
-                <a href = "#" class="btn">Update</a>
+                <p>Type: <?php echo $detailItem->get_type(); ?></p>        
             </div> 
+        </div>
+        <div class="row">
+            <a href = "#" class="btn">Update</a>
         </div>
     </div> 
 
