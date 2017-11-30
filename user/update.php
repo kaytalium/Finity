d2b8b192d5b1c8fbd46d9a10d70fb703e80baf9d
Tajhna McCourtie <tmc253@hotmail.com>
2017-11-30 13:44:30 -0500

    Added user

---
+++ user/update.php
@@ -0,0 +1,34 @@
+<form class="userform">
+    <div class="user-wrapper">
+        <div class="imgprofile">
+        <img src="../image/camera.png">
+        </div>
+
+        <div class=" userinfo">
+            <div class="row">
+                <div class="fn">First Name</div>
+            </div>    
+        
+            <div class="row">
+                <div class="ln">Last Name</div>
+            </div>
+
+            <div class="row">
+                <div class="email">Email</div>
+            </div>
+
+            <div class="row">   
+                <div class ="passwrd">Password</div>  
+            </div>
+
+            <div class="row">
+                <div class="ustype">User Type</div>
+            </div>
+
+            <div class="row">
+                <a href = "#" class="btn">Edit</a>
+                <a href = "#" class="btn">Cancel</a>
+            </div>
+        </div>       
+    </div>
+</form>     
