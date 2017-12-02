8ae3f495955d01ffdaf2e883c45346bf7772f2eb
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-12-02 12:07:41 -0500

    update header look and feel

--- header.php
+++ header.php
@@ -5,19 +5,28 @@
         <div class="co-name">Finity<br> Inventory<br> System</div>
     </logo>
 
-    <menu>
+    <search>
+        <form action="">
+            <input type="text" name="searchbox" placeholder="Search for items here: e.g Soccer">
+        </form>
+    </search>
+
+    <menunav>
         <ul>
             <li><a href="admin.php">Admin</a></li>
-            <li><a href="product.php">Product</a></li>
+            <li><a href="product.php">Products</a></li>
         </ul>
-    </menu>
+    </menunav>
    
     <profile>
-        <div class="profile-holder">
+        <div class="profile-holder" title="Your Profile">
             <img src="image/users/default.png"/>
         </div> 
+        <div class="info">
+            Hello, Kimberley           
+        </div>
         <div class="logout">
-
+        <a href="#"><i class="fa fa-sign-out fa-lg" aria-hidden="true" title="logout">Exit</i><p class ="text"></p></a>
         </div>
         <profile-detail>
             <?php include 'user/update.php'; ?>
--- product.php
+++ product.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/font/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/finity.css">
     <link rel="stylesheet" href="css/product.css">
     <link rel="stylesheet" href="css/detail.css">
     <link rel="stylesheet" href="css/sidebar.css">
-    <link rel="stylesheet" href="css/font/css/font-awesome.min.css">
+    
 
     <title>Product</title>
     <script src="js/jquery-3.2.1.min.js"></script>
