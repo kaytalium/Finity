92c107b7342d6edc24a896adc3ff73a7020a2fe5
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-12-02 20:44:20 -0500

    redesign user profile and add functionalities

--- js/details-controller.js
+++ js/details-controller.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     //setting up referrence to containers
+
+    //Item Detail edit
     $display_info   = $("#display_info")
     $edit_info      = $(".edit_info")
+
     
     //btns
     $editBtn    = $("#edit-btn")
@@ -17,6 +20,76 @@ $(document).ready(function(){
         hideShow($edit_info, $display_info)
     })
 
+    //user profile edit
+    $userDetail         = $(".user-detail")
+    $edituserinfo       = $(".edituserinfo")
+
+    //buttons 
+    $editProfile        = $("#edit-user-profile")
+    $cancelUserUpdate   = $("#cancel-user-update")
+
+    //funcationalies
+    $editProfile.click(function(){
+        hideShow($userDetail,$edituserinfo)
+    })
+
+    $cancelUserUpdate.click(function(e){
+        e.preventDefault()
+        hideShow($edituserinfo,$userDetail)
+    })
+
+    //Change Password functionalities
+
+    //change password container
+    $chngPassword       = $(".change-password")
+
+    //button
+    $chngPswdbtn        = $("#change-password")
+    $savePassword       = $("#save-password")
+    $cancelPassword     = $("#cancel-password")
+
+    //Password textbox
+    $newpwd             = $("#newPwd")
+    $retypePwd          = $("#rtyPwd")
+
+    //functions
+    $chngPswdbtn.click(function(){
+        
+        $chngPassword.toggle(600, function(){
+            $newpwd.val("")
+            $retypePwd.val("")
+        })
+
+    })
+
+    //Cancel Password
+    $cancelPassword.click(function(){
+        $newpwd.removeClass('ok').removeClass("error")
+        $retypePwd.removeClass('ok').removeClass("error")
+        $chngPassword.hide(300, function(){
+            $newpwd.val("")
+            $retypePwd.val("")
+        })
+    })
+
+    //Save New Password
+    $savePassword.click(function(){
+        //check to see if password match before closing
+        
+        if($newpwd.val() != $retypePwd.val() || $newpwd.val().length == 0){
+            $newpwd.addClass('error')
+            $retypePwd.addClass('error')
+        }else{
+            $newpwd.addClass('ok').removeClass("error")
+            $retypePwd.addClass('ok').removeClass("error")
+            $chngPassword.hide(600, function(){
+                $newpwd.val("")
+                $retypePwd.val("")
+            })
+        }
+        
+    })
+
 });
 
 /**
@@ -25,6 +98,6 @@ $(document).ready(function(){
  * @param {*container that you want to show} showCon 
  */
 function hideShow(hideCon, showCon){
-    hideCon.hide()
-    showCon.show()
+    hideCon.fadeOut(600).delay(300)
+    showCon.delay(600).fadeIn(300)
 }
--- user/profile.php
+++ user/profile.php
@@ -1,41 +1,80 @@
-<link rel="stylesheet" href="css/user_update.css"> 
+<link rel="stylesheet" href="css/user_profile.css"> 
 
-<form class="userform"> 
     <div class="user-wrapper">
-        <div class="imgprofile">
-        <img src="image/alicia.jpg">
+        <div class="userinfo">
+            <div class="profile-image">
+                <div class="imgprofile">
+                    <img src="image/alicia.jpg">
+                </div>
+                <div class="imageController">
+                    <button class="btn lg" id="editusphoto">Change Picture</button>
+                </div>
+            </div>
+        
+            <div class="user-detail">
+                <table class="info-table">
+                    <tr>
+                        <td>Name:</td>
+                        <td>Kelly Logan</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td>goallie@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>User Type:</td>
+                        <td>Administrator</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div class="row button">
+                                <button class="btn" id="edit-user-profile">Edit Profile</button>
+                            <!-- <button class="btn" id="canusprofile">Cancel</button> -->
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="edituserinfo">
+                <form action="" method="POST" class="edituserinfoform"> 
+                    <table>
+                        <tr>
+                            <td>Firstname:</td>
+                            <td><input type="text" placeholder="Firstname" class="input" required/></td>
+                        </tr>
+                        <tr>
+                            <td>Lastname:</td>
+                            <td><input type="text" placeholder="Lastname" class="input" required/></td>
+                        </tr>
+                        <tr>
+                            <td>Email:</td>
+                            <td><input type="email" placeholder="Email" class="input" required/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="submit" value="Update" class="btn"></td>
+                            <td><button id="cancel-user-update" class="btn">Cancel</button></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
         </div>
 
-        <div class=" userinfo">
-            <div class="row">
-                <div class="text">First Name: Kelly</div>
-                <div class="txtbox hide"><input class="fntxt" type="text" placeholder="first name"></div>
-            </div>    
         
-            <div class="row">
-                <div class="text">Last Name: Logan</div>
-                <div class="txtbox hide"><input class="fntxt" type="text" placeholder="last name"></div>
-            </div>
-
-            <div class="row">
-                <div class="text">Email: goallie@gmail.com</div>
-                <div class="txtbox hide"><input class="fntxt" type="text" placeholder="email"></div>
-            </div>
-
-            <div class="row">   
-                <!-- <div class ="text">Change Password: </div>   -->
-                <div class="txtbox hide"><input class="fntxt" type="text" placeholder="c password"></div>
-            </div>
-
-            <div class="row">
-                <div class="text">User Type: Administrator</div>
-                <div class="txtbox hide"><input class="fntxt" type="text" placeholder="usertype"></div>
-            </div>
-
-            <div class="row buttons">
-                <button class="btn" id="editusprofile">Edit</button>
-                <button class="btn" id="canusprofile">Cancel</button>
-            </div>
-        </div>       
-    </div>
-</form>     
+        <table>
+            <tr>
+                <td colspan="2"><div class="text clickable" id="change-password">Change Password</div></td>
+                <td></td>
+            </tr>
+            <tr class="change-password">
+                <td><input type="password" class="input" placeholder="New Password" id="newPwd"></td>
+                <td><input type="password" class="input" placeholder="Retype Password" id="rtyPwd"></td>
+                <td>
+                    <i class="fa fa-times-circle fa-lg" aria-hidden="true" id="cancel-password"></i>
+                    <i class="fa fa-check-circle fa-lg" aria-hidden="true" id="save-password"></i>
+                </td>
+            </tr>
+        </table>
+        
+        
+    </div>   
