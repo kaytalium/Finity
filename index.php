5e6687625c080eb0fc5700af177296865296fde3
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-12-03 09:31:53 -0500

    add login design to project

--- index.php
+++ index.php
@@ -8,10 +8,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/finity.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Inventory Management System</title>
 </head>
-<body>
-<h1>Finity Inventory</h1>
+<?php
+    $islogin = false;
+    $isSignup = false;
 
-</body>
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        
+        $requestor = (isset($_GET['v'])?$_GET['v']:'');
+        switch($requestor){
+            case 'signup':
+            $isSignup = true;
+            break;
+
+            case 'login';
+            $islogin = true;
+            break;
+
+            default:
+            $islogin = true;
+        }
+    }else{
+        $islogin = true;
+    }
+?>
+
+    <body>
+        <?php 
+            if($islogin)
+            include 'login/login.php';
+
+            if($isSignup)
+            include 'login/signup.php';
+
+        ?>
+
+    </body>
 </html>
---
+++ login/login.php
@@ -0,0 +1,27 @@
+<div class="wrapper">
+    <div class = "loginpage">
+        <form method="POST" action="#">
+            
+            <div class="title">
+                <img src ="image/logo.png" class="logo-img">    
+                Finity
+                <span class="small-title">Inventory System</span>
+            </div>
+            <div class="form-input">
+                <label>Email:</label>
+                <input type="email" placeholder="Email Address" name="emailad" required>
+            </div>
+            
+            <div class="form-input">
+                <label>Password:</label>
+                <input type="password" placeholder="Enter Password" name="pwd" required>
+            </div>
+
+            <button type="login" name="login" value="loginbtn" class="btn">login</button>
+            <p>
+                Not a member? <a href="<?php echo $_SERVER['PHP_SELF'].'?v=signup';?>">Sign up</a> 
+            </p>    
+        </form>
+    </div>
+</div>
+        
