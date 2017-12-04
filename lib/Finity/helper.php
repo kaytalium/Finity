e7b29b0af2939e281206ece3b09803157ca11bd8
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-12-04 08:47:32 -0500

    add more general functions

--- lib/Finity/helper.php
+++ lib/Finity/helper.php
@@ -22,9 +22,7 @@ function toMoney($val='')
     }
 }
 
-function cleanPostDataFromUser(){
-    
-    $arg  = unserialize(LOGINVAR);
+function cleanPostDataFromUser($arg = array()){
         foreach($arg as $key=>$field){
             $arg[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
         }
@@ -32,18 +30,57 @@ function cleanPostDataFromUser(){
 
 }
 
-function logincheckCleanDataFromUser($arg = array()){
-    $error = array("?"=>true,"errors"=>"");
+function checkCleanDataFromUser($arg = array()){
+    $error = array("?"=>true,"errors"=>array());
     foreach($arg as $key=>$val){
         if($key=='email' && !filter_var($val, FILTER_VALIDATE_EMAIL)){
             $error['?'] = false;
-            $error['errors'] = array($key=>"invalid email");
+            $error['errors'][$key] = "Invalid email";
         }
 
         if($key=='pwd' && strlen($val)<8){
             $error['?'] = false;
-            $error['errors'] = array($key=>"Password length too small");
+            $error['errors'][$key] = "Password length too short";
         }
+
+        if($key=='Rpwd' && $val != $arg['pwd'] || $key=='Rpwd' && empty($val)){
+            $error['?'] = false;
+            $error['errors'][$key] = "This password does not match";
+        }
+
+        if($key=='fname' && empty($val) || $key=='fname' && !preg_match('/^[a-zA-Z]+$/', trim($val))){
+            $error['?'] = false;
+            $error['errors'][$key] = "Invalid value";
+        }
+
+        if($key=='lname' && empty($val) || $key=='lname' && !preg_match('/^[a-zA-Z]+$/', trim($val))){
+            $error['?'] = false;
+            $error['errors'][$key] = "Invalid value";
+        }
+
+        if($key=='bday' && empty($val)){
+            $error['?'] = false;
+            $error['errors'][$key] = "Invality date";
+        }
+
     }
     return $error;
+}
+
+function isClass($arg){
+    if(isset($arg))
+        return 'error';
+    else
+        return 'ok';
+}
+
+function isShow($arg){
+    if(isset($arg))
+        return 'show';
+    else
+        return 'hide';
+}
+
+function isVarSet($arg = array(), $str){
+    return (isset($arg[$str])?$arg[$str]:'');
 }
