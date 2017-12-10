be430b123877aa892a2512263224f5c3e95e16c6
Tajhna McCourtie <tmc253@hotmail.com>
2017-12-09 19:17:38 -0500

    added styling and layout for report

--- report/management_report.php
+++ report/management_report.php
@@ -1,5 +1,18 @@
 <div class="m_report_container">
     <h1>Management Report</h1>
+    <div class="left">
+        <p>Select a Report</p> 
+        <div class="space1"><button class="quan_btn">Quantity Report</button></div>
+        <div class="space2"><button class="sales_btn">Sales Report</button></div>
+        <div class="space3"><button class="inven_btn">Inventory</button></div>
+       <div class="space4"><button class="purs_btn">Purchases</button></div>
+    </div>
 
+    <div class ="right">
+        <div class="report_container"/>
+            <div class="title_header"><h3>Finity Inventory System</h3></div>
+            <div class ="date_gen"><h4>Date Generated:</h4></div>
+     </div>    
+  </div>    
 
 </div>
