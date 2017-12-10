9f7385058804dc7e37127e72b645b292b6ab8a08
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-12-09 19:30:52 -0500

    fix layout issue

--- report/management_report.php
+++ report/management_report.php
@@ -2,14 +2,14 @@
     <h1>Management Report</h1>
     <div class="left">
         <p>Select a Report</p> 
-        <div class="space1"><button class="quan_btn">Quantity Report</button></div>
-        <div class="space2"><button class="sales_btn">Sales Report</button></div>
-        <div class="space3"><button class="inven_btn">Inventory</button></div>
-       <div class="space4"><button class="purs_btn">Purchases</button></div>
+        <div class="space"><button class="r_btn" id="quan_btn">Quantity Report</button></div>
+        <div class="space"><button class="r_btn" id="sales_btn">Sales Report</button></div>
+        <div class="space"><button class="r_btn" id="inven_btn">Inventory</button></div>
+        <div class="space"><button  class="r_btn"  id="purs_btn">Purchases</button></div>
     </div>
 
     <div class ="right">
-        <div class="report_container"/>
+        <div class="report_container">
             <div class="title_header"><h3>Finity Inventory System</h3></div>
             <div class ="date_gen"><h4>Date Generated:</h4></div>
      </div>    
