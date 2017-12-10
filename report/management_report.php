09803ead490fb63685a167addbe2097182929f6d
Tajhna McCourtie <tmc253@hotmail.com>
2017-12-09 20:41:27 -0500

    edited css

--- report/management_report.php
+++ report/management_report.php
@@ -11,7 +11,56 @@
     <div class ="right">
         <div class="report_container">
             <div class="title_header"><h3>Finity Inventory System</h3></div>
-            <div class ="date_gen"><h4>Date Generated:</h4></div>
+                <div class ="date_gen"><h4>Date Generated:</h4></div>
+               <div><h4>Name: </h4></div>
+            <div class="tbl-header">
+                <table cellpadding="0"  cellspacing="0" border="0">
+                    <thead>
+                        <tr>
+                            <th id="sum-header">Summary</th>
+                        </tr>  
+                        <tr>  
+                            <th>Heading 1</th>
+                            <th>Heading 2</th>
+                            <th>Heading 3</th>
+                        </tr>  
+                   </thead> 
+               </table> 
+            </div> 
+            
+            <div class="tbl-content">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <tbody>
+                        <tr> 
+                            <td>Content 1</td> 
+                            <td>Content 2</td> 
+                            <td>Content 3</td> 
+                        </tr>
+                        <tr>
+                            <td>Content 4</td>
+                            <td>Content 5</td> 
+                            <td>Content 6</td> 
+                        </tr>
+                        <tr>
+                            <td>Content 7</td> 
+                            <td>Content 8</td> 
+                            <td>Content 9</td> 
+                        </tr>
+                        <tr>
+                            <td>Content 10</td>
+                            <td>Content 11</td> 
+                            <td>Content 12</td>
+                        </tr>    
+                        <tr>
+                            <td>Content 13</td>
+                            <td>Content 14</td> 
+                            <td>Content 15</td>
+                        </tr>    
+                    </tbody>
+                </table> 
+            </div>             
+
+            
      </div>    
   </div>    
 
